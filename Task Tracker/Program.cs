d4b7c613b935a18abd8da4186b1b91757744996c
Brian Gelhorn <briangelhorn@gmail.com>
2026-03-20 23:18:16 -0300

    Add Task

---
+++ Task Tracker/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+using Microsoft.Win32.SafeHandles;
+
+namespace Task_Tracker;
+using System.Text.Json;
+using System.Text;
+class Program
+{
+    private const string TASKDATABASEDIR = "taskDatabase.json";
+    private static readonly string[] STATUS = ["todo", "in-progress", "done"];
+
+    public class TaskItem
+    {
+        public int id { get; set; }
+        public string description { get; set; }
+        public string status  { get; set; }
+        public DateTime createdAt  { get; set; }
+        public DateTime updatedAt { get; set; }
+    }
+    
+    static void Main(string[] args)
+    {
+        if (!File.Exists(TASKDATABASEDIR))
+        {
+            File.WriteAllText(TASKDATABASEDIR, "[\n]");
+        }
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Usage: task-tracker <command> [arguments]");
+            return;
+        }
+
+        switch (args[0])
+        {
+            case "help":
+                //TODO: Implement help message
+                break;
+            case "add":
+                //TODO: Add check of arguments
+                AddTask(args[1]);
+                break;
+            case "update":
+                //TODO: Implement update logic
+                break;
+            case "delete":
+                //TODO: Implement delete logic
+                break;
+            case "list":
+                //TODO: Implement list logic
+                break;
+            default:
+                Console.WriteLine("Unknown command " + args[0]);
+                break;
+        }
+    }
+
+    private static string _taskDataBase = File.ReadAllText(TASKDATABASEDIR);
+    private static List<TaskItem> GetAllTasks() => JsonSerializer.Deserialize<List<TaskItem>>(_taskDataBase)!;
+
+    private static void UpdateTaskJson(List<TaskItem> taskItem)
+    {
+        string serializedTaskList = JsonSerializer.Serialize(taskItem, new JsonSerializerOptions{WriteIndented = true});
+        File.WriteAllText(TASKDATABASEDIR, serializedTaskList);
+    }
+
+    public static void AddTask(string description)
+    {
+        int id = GetAllTasks().Count;
+        string status = STATUS[0];
+        DateTime createdAt = DateTime.Now;
+        DateTime updatedAt = createdAt;
+        TaskItem newTask = new TaskItem
+        {
+            id = id,
+            description = description,
+            status = status,
+            createdAt = createdAt,
+            updatedAt = updatedAt
+        };
+        List<TaskItem> tasksToUpdate = GetAllTasks();
+        tasksToUpdate.Add(newTask);
+        UpdateTaskJson(tasksToUpdate);
+    }
+}
